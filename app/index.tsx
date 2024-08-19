8a9437deb7d37ffe26fd6df91071b0b743e74f4b
Nicoka11 <nicoka11@yahoo.fr>
2024-08-19 13:09:37 +0200

    feat: basic login interface with react hook form validation

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,100 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { useForm, Controller } from "react-hook-form";
+import { PrimaryButton } from "@/components/Button";
 
+interface FormData {
+  password: string;
+  email: string;
+}
 export default function Index() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    defaultValues: {
+      password: "",
+      email: "",
+    },
+  });
+
+  const onSubmit = (data: FormData) => console.log(data);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 12,
-      }}
-    >
-     <Text>Welcome to default page</Text>
+    <View style={styles.container}>
+      <View style={styles.title}>
+        <Text style={{ ...styles.text, fontSize: 48 }}>PURE</Text>
+        <Text style={styles.text}>todo</Text>
+      </View>
+      <Controller
+        name="email"
+        control={control}
+        rules={{
+          required: "Email is required",
+          pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            textContentType="emailAddress"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+      />
+      <Controller
+        name="password"
+        control={control}
+        rules={{
+          required: "Password is required",
+          minLength: {
+            value: 6,
+            message: "Password must be at least 6 characters long",
+          },
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            textContentType="password"
+            secureTextEntry
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+      />
+      <PrimaryButton title="Login" onPress={handleSubmit(onSubmit)} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 12,
+    paddingHorizontal: 32,
+  },
+  input: {
+    width: "100%",
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+  },
+  title: {
+    flexDirection: "row",
+    width: "auto",
+    alignItems: "flex-end",
+  },
+  text: {
+    fontFamily: "ClashDisplay-Variable",
+    fontWeight: "800",
+    fontSize: 32,
+  },
+});
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,20 +1,73 @@
-import { Button, ButtonProps, StyleSheet } from "react-native";
+import React, { useRef, useState } from "react";
+import {
+  Pressable,
+  Text,
+  StyleSheet,
+  GestureResponderEvent,
+  Animated,
+} from "react-native";
 
-export interface CustomButtonProps extends ButtonProps {}
+interface CustomButtonProps {
+  title: string;
+  onPress: (event: GestureResponderEvent) => void;
+}
 
-export const ButtonPrimary = (props: CustomButtonProps) => {
-  return <Button color="black" {...props} />;
+export const PrimaryButton: React.FC<CustomButtonProps> = ({
+  title,
+  onPress,
+}) => {
+  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  const handlePressIn = () => {
+    Animated.timing(animatedValue, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const handlePressOut = () => {
+    Animated.timing(animatedValue, {
+      toValue: 0,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const backgroundColor = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["black", "#333"],
+  });
+
+  const textColor = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["white", "#ccc"],
+  });
+
+  return (
+    <Pressable
+      style={{ width: "100%" }}
+      onPress={onPress}
+      onPressIn={handlePressIn}
+      onPressOut={handlePressOut}
+    >
+      <Animated.View style={[styles.button, { backgroundColor }]}>
+        <Animated.Text style={[styles.text, { color: textColor }]}>
+          {title}
+        </Animated.Text>
+      </Animated.View>
+    </Pressable>
+  );
 };
 
 const styles = StyleSheet.create({
-  primary: {
-    backgroundColor: "black",
-    color: "white",
+  button: {
     borderRadius: 99,
     alignItems: "center",
     paddingVertical: 8,
     width: "100%",
   },
+  text: {
+    color: "white",
+  },
 });
-
-console.log(styles);
